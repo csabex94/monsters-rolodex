d5e342180fdd555e8e2d76f4bf50cd0fa4da8187
unknown <csabex94@yahoo.com>
2019-07-16 19:27:07 +0300

    Updates

---
+++ precache-manifest.6cc3814b75b8869b182d6d0a2cde82c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb49e6647af44ebd938487f10e3d82d4",
+    "url": "/csabex94/monsters-rolodex.git/index.html"
+  },
+  {
+    "revision": "efce8236cc4155d906d0",
+    "url": "/csabex94/monsters-rolodex.git/static/css/main.8195180d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/csabex94/monsters-rolodex.git/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "efce8236cc4155d906d0",
+    "url": "/csabex94/monsters-rolodex.git/static/js/main.2e11114e.chunk.js"
+  },
+  {
+    "revision": "e8c76aff1a10c2855ed5",
+    "url": "/csabex94/monsters-rolodex.git/static/js/runtime~main.faab6912.js"
+  }
+]);
